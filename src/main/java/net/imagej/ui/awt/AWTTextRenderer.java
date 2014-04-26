42ff6527bd1639176ff491381803bfe40a0b9bd0
Curtis Rueden <ctrueden@wisc.edu>
2014-04-26 15:36:16 -0500

    Update project license blurbs
    Accomplished using:
       mvn license:update-file-header license:update-project-license

--- src/main/java/net/imagej/ui/awt/AWTTextRenderer.java
+++ src/main/java/net/imagej/ui/awt/AWTTextRenderer.java
@@ -1,10 +1,11 @@
 package net.imagej.ui.awt;
 /*
  * #%L
- * SciJava UI components for Java AWT.
+ * ImageJ software for multidimensional image processing and analysis.
  * %%
- * Copyright (C) 2011 - 2014 Board of Regents of the University of
- * Wisconsin-Madison.
+ * Copyright (C) 2009 - 2014 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
